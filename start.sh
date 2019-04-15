cab9da2556fe545cad6f7c1c1d6c9d5776a9436b
cretubogdan <cretumbogdan@gmail.com>
2019-04-16 01:54:07 +0300

    Small modifies

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm logs
-rm -rf __pycache__
 export LOGGER_PATH=/home/bogcretu/Desktop/workspace/NLP_fakenews/logs
 export DATASET_PATH=/home/bogcretu/Desktop/task2/datasets/liar_dataset/test.tsv
 python3 Main.py Logger.py DbManager.py WorkPool.py Reader.py
+rm -rf __pycache__
