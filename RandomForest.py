4e16369614a161a16c4f8a042d1d78399a745750
cretubogdan <mihnea_bogdan.cretu@stud.acs.upb.ro>
2019-04-30 04:05:15 +0300

    Change file name

--- RandomForest.py
+++ RandomForest.py
@@ -18,69 +18,99 @@ QUERY = 3
 USER = 4
 TEXT = 5
 
-def DoClean(review):
-    review = re.sub("[^a-zA-Z]"," ", review)
-    review = review.lower().split()
+estimators = 500
+stop_words = None
+vectorizer = None
+data_features = None
+forest = None
 
-    stops = set(stopwords.words("english"))
-    words = [w for w in review if not w in stops]
+l = Logger()
+wp = WorkPool()
+r = Reader()
 
-    review = " ".join(words)
+def do_init():
+    global l, stop_words
+    l.log(Severity.INFO, "Random forest: started init")
+    stop_words = set(stopwords.words('english'))
+    l.log(Severity.INFO, "Random forest: finished init")
 
-    return review
+def do_read():
+    global l, r
+    l.log(Severity.INFO, "Random forest: started reading")
+    r.read()
+    l.log(Severity.INFO, "Random forest: finished reading")
+
+@wp.do_tasks
+def do_clean_helper(data):
+    for d in data:
+        d[TEXT] = re.sub("[^a-zA-Z]"," ", d[TEXT]).lower().split()
+        words = [w for w in d[TEXT] if not w in stop_words]
+        d[TEXT] = " ".join(words)
+
+def do_clean():
+    global l, r
+    l.log(Severity.INFO, "Random forest: started cleaning")
+    do_clean_helper(r.train)
+    do_clean_helper(r.test)
+    l.log(Severity.INFO, "Random forest: finished cleaning")
+
+@wp.do_tasks
+def get_text(data):
+    to_return = []
+    for d in data:
+        to_return.append(d[TEXT])
+    return to_return
+
+@wp.do_tasks
+def get_polarity(data):
+    to_return = []
+    for d in data:
+        to_return.append(d[POLARITY])
+    return to_return
+
+
+def do_dict():
+    global l, data_features, r, vectorizer
+    l.log(Severity.INFO, "Random forest: started creating dict")
+    vectorizer = CountVectorizer()
+    column_text = get_text(r.train)
+    data_features = vectorizer.fit_transform(column_text).toarray()
+    l.log(Severity.INFO, "Random forest: finished creating dict")
+
+def do_train():
+    global l, forest, r, estimators
+    l.log(Severity.INFO, "Random forest: started train")
+    forest = RandomForestClassifier(n_estimators = estimators)
+    data_labels = get_polarity(r.train)
+    forest = forest.fit(data_features, data_labels)
+    l.log(Severity.INFO, "Random forest: finished traing")
+
+def get_prediction_percent(predict, real):
+    global l
+    value = len([i for i, j in zip(predict, real) if i == j])
+    value = float(value * 100 / len(predict))
+    l.log(Severity.RESULT, "Random forest: predict percent: {0}".format(value))
+
+def do_test():
+    global l, r, vectorizer
+    l.log(Severity.INFO, "Random forest: started testing")
+    column_text = get_text(r.test)
+    test_labels = get_polarity(r.test)
+    test_features = vectorizer.transform(column_text).toarray()
+    results = forest.predict(test_features)
+
+    get_prediction_percent(list(results), test_labels)
+    
+    l.log(Severity.INFO, "Random forest: finished testing")
 
 def main():
-    csv = Reader()
-    csv.Read()
-
-    header = csv.GetHeaderTrain()
-    train = csv.GetBodyTrain()
-    test = csv.GetBodyTest()
-
-    print("Am terminat de citit")
-
-    clean_train = []
-    train_labeled = []
-    for i in range(len(train)):
-        clean_train.append(DoClean(train[i][TEXT]))
-        train_labeled.append(train[i][POLARITY])
-
-    vectorizer = CountVectorizer()
-    train_data_features = vectorizer.fit_transform(clean_train)
-    train_data_features = train_data_features.toarray()
-
-    print("Am terminat de facut dictionarul")
-
-    forest = RandomForestClassifier(n_estimators = 5000)
-    forest = forest.fit(train_data_features, train_labeled)
-
-    print("Am terminat de aplicat random forest")
-
-
-    clean_test = []
-    test_labeled = []
-    for i in range(len(test)):
-        clean_test.append(DoClean(test[i][TEXT]))
-        test_labeled.append(test[i][POLARITY])
-
-    print("Am terminat de testat datele")
-
-    test_data_features = vectorizer.transform(clean_test).toarray()
-
-    result = forest.predict(test_data_features)
-
-    
-    if len(result) != len(test):
-        print("AU LUNGIMI DIFERITE")
-
-    shots = 0
-    for i in range(len(result)):
-        if result[i] == test[i][POLARITY]:
-            shots += 1
-
-    print("Am o probabilitate de " + str(((shots / len(result)) * 100)))
-
-
+    do_init()
+    do_read()
+    do_clean()
+    do_dict()
+    do_train()
+    do_test()
 
 if __name__ == '__main__':
     main()
+    wp.abort()
--- start_randomforest.sh
+++ start_randomforest.sh
@@ -2,5 +2,5 @@ rm logs
 export LOGGER_PATH=/Users/mihnea-bogdancretu/Desktop/licenta/NLP_fakenews/logs
 export DATASET=/Users/mihnea-bogdancretu/Desktop/licenta/trainingandtestdata/dataset.csv
 export DEBUG=FALSE
-python3 RandomForest2.py modules/Logger.py modules/DbManager.py modules/WorkPool.py modules/Reader.py
+python3 RandomForest.py modules/Logger.py modules/DbManager.py modules/WorkPool.py modules/Reader.py
 rm -rf __pycache__
